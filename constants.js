960278ce2f8e1ab50cd76dd309ac9ad87ef831c5
Kevin Blanco <kevinjblanco@gmail.com>
2022-12-29 11:39:34 -0500

    constants created for columns/rows/blocks/colors

---
+++ constants.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const COLS = 10
+const ROWS = 20
+const BLOCK_SIZE = 30
+const LINES_PER_LEVEL = 10
+const COLORS = [
+  'none',
+  'cyan',
+  'blue',
+  'orange',
+  'yellow',
+  'green',
+  'purple',
+  'red'
+]
+Object.freeze(COLORS)
