0676c879d551287711795bd347b0cf3421e11e8f
Kevin Blanco <kevinjblanco@gmail.com>
2022-12-29 11:48:25 -0500

    const created for keys, points, and levels

--- constants.js
+++ constants.js
@@ -54,3 +54,51 @@ const SHAPES = [
     [0, 0, 0]
   ]
 ]
+Object.freeze(SHAPES)
+
+const KEY = {
+  ESC: 27,
+  SPACE: 32,
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40,
+  P: 80
+}
+Object.freeze(KEY)
+
+const POINTS = {
+  SINGLE: 100,
+  DOUBLE: 300,
+  TRIPLE: 500,
+  TETRIS: 800,
+  SOFT_DROP: 1,
+  HARD_DROP: 2
+}
+Object.freeze(POINTS)
+
+const LEVEL = {
+  0: 800,
+  1: 720,
+  2: 630,
+  3: 550,
+  4: 470,
+  5: 380,
+  6: 300,
+  7: 220,
+  8: 130,
+  9: 100,
+  10: 80,
+  11: 80,
+  12: 80,
+  13: 70,
+  14: 70,
+  15: 70,
+  16: 50,
+  17: 50,
+  18: 50,
+  19: 30,
+  20: 30
+  // 29+ is 20ms
+}
+Object.freeze(LEVEL)
