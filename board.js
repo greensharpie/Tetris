033e3233db05628ccceaf630b98f2713865a5ea6
Kevin Blanco <kevinjblanco@gmail.com>
2022-12-29 20:03:42 -0500

    play and reset board functions created

---
+++ board.js
@@ -0,0 +1,8 @@
+class Board {
+  reset() {
+    this.grid = this.getEmptyBoard()
+  }
+  getEmptyBoard() {
+    return Array.from({ length: ROWS }, () => Array(COLS).fill(0))
+  }
+}
--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-//Accessing physical file system
-const fs = require('fs')
-
-// Reading the file (Synchronous function)
-/* const data = fs.readFileSync(file, char encoding); */
-const data = fs.readFileSync('./whereDatasAreStored.txt', {
-  encoding: 'utf8',
-  flag: 'r'
-})
-
 const canvas = document.getElementById('board')
 
 const ctx = canvas.getContext('2d')
@@ -16,3 +6,10 @@ ctx.canvas.width = COLS * BLOCK_SIZE
 ctx.canvas.height = ROWS * BLOCK_SIZE
 
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE)
+
+let board = new Board()
+
+function play() {
+  board.reset()
+  console.table(board.grid)
+}
