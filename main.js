9b50e11300f6936e360a25ac49015dad5fc6ac08
Kevin Blanco <kevinjblanco@gmail.com>
2022-12-30 00:51:13 -0500

    pieces display with time interval

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('board')
 const ctx = canvas.getContext('2d')
 const canvasNext = document.getElementById('next')
-const ctxNext = canvas.getContext('2d')
+const ctxNext = canvasNext.getContext('2d')
 
 let accountValues = {
   score: 0,
@@ -10,7 +10,7 @@ let accountValues = {
 }
 
 function updateAccount(key, value) {
-  let element = getElementById(key)
+  let element = document.getElementById(key)
   if (element) {
     element.textContent = value
   }
@@ -82,12 +82,12 @@ function resetGame() {
   account.lines = 0
   account.level = 0
   board.reset()
-  time = { start: 0, elapsed: 0, level: LEVEL[accounts.level] }
+  time = { start: 0, elapsed: 0, level: LEVEL[account.level] }
 }
 
 function play() {
   resetGame()
-  time.start = perform.now()
+  time.start = performance.now()
   if (requestId) {
     cancelAnimationFrame(requestId)
   }
@@ -101,7 +101,7 @@ function play() {
   // board.piece = piece
 }
 
-function animate() {
+function animate(now = 0) {
   time.elapsed = now - time.start
   if (time.elapsed > time.level) {
     time.start = now
