8cda3f81a72ac94daf2ea08105e815b7dbbc9a63
Kevin Blanco <kevinjblanco@gmail.com>
2022-12-29 21:50:12 -0500

    functions created to stay inside walls and bove floor

--- board.js
+++ board.js
@@ -5,4 +5,22 @@ class Board {
   getEmptyBoard() {
     return Array.from({ length: ROWS }, () => Array(COLS).fill(0))
   }
+
+  valid(p) {
+    return p.shape.every((row, dy) => {
+      return row.every((value, dx) => {
+        let x = p.x + dx
+        let y = p.y + dy
+        return this.insideWalls(x) && this.aboveFloor(y)
+      })
+    })
+  }
+
+  insideWalls(x) {
+    return x >= 0 && x < COLS
+  }
+
+  aboveFloor(y) {
+    return y <= ROWS
+  }
 }
