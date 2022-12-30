b6bed91016f5f3fc4138204ef17dac78150615b2
Kevin Blanco <kevinjblanco@gmail.com>
2022-12-29 22:20:38 -0500

    added rotation to piece

--- main.js
+++ main.js
@@ -22,7 +22,8 @@ const moves = {
   [KEY.LEFT]: (p) => ({ ...p, x: p.x - 1 }),
   [KEY.RIGHT]: (p) => ({ ...p, x: p.x + 1 }),
   [KEY.DOWN]: (p) => ({ ...p, y: p.y + 1 }),
-  [KEY.SPACE]: (p) => ({ ...p, y: p.y + 1 }) //hard drop
+  [KEY.SPACE]: (p) => ({ ...p, y: p.y + 1 }), //hard drop
+  [KEY.UP]: (p) => board.rotate(p)
 }
 
 document.addEventListener('keydown', (event) => {
